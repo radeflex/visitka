d540eeb9528640e10a58230c019abb17c32aa76e
Radeflex <maks.krav61@gmail.com>
2024-10-20 15:09:44 +0300

    some changes

--- src/components/ProjectEntry/ProjectEntry.jsx
+++ src/components/ProjectEntry/ProjectEntry.jsx
@@ -4,7 +4,7 @@ import Divider from "@mui/material/Divider";
 import { Card, Box, Stack, Chip, Typography, Button } from "@mui/material";
 import gift from "../../img/gift-box.png";
 
-export default function ProjectEntry({ langs }) {
+export default function ProjectEntry({ langs, author, title, repoLink }) {
   return (
     <Card
       variant="outlined"
@@ -20,17 +20,25 @@ export default function ProjectEntry({ langs }) {
         <Divider />
         <Box sx={{ p: 5 }}>
           <Stack direction="column" spacing={0.5} alignItems="flex-start">
-            <Typography variant="h5">Freelance project</Typography>
-            <Typography variant="h6">Author: author</Typography>
+            <Typography variant="h5">{title}</Typography>
+            <Typography variant="h6">Author: {author}</Typography>
             <Typography variant="h6">Languages:</Typography>
           </Stack>
-          <Stack direction="row" sx={{justifyContent: "space-between", marginTop: "4%"}}>
+          <Stack
+            direction="row"
+            sx={{ justifyContent: "space-between", marginTop: "4%" }}
+          >
             <Stack direction="row" spacing={1}>
-              <Chip label="Lang1" size="medium" />
-              <Chip label="Lang2" size="medium" />
-              <Chip label="Lang3" size="medium" />
+              {langs.map((el, id) => (
+                <Chip label={el} size="medium" key={id} />
+              ))}
             </Stack>
-            <Button variant="contained" href="https://github.com/radeflex/freelrepos">Go to</Button>
+            <Button
+              variant="contained"
+              href={repoLink}
+            >
+              Go to
+            </Button>
           </Stack>
         </Box>
       </Box>
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,18 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Projects.css";
 import ProjectEntry from "../../components/ProjectEntry/ProjectEntry";
 import { Container, Pagination } from "@mui/material";
 
 export default function Projects() {
-  const [data, setData] = useState();
+  const [title, setTitle] = useState();
+  const [author, setAuthor] = useState();
+  const [langs, setLangs] = useState([]);
+  const [repo, setRepo] = useState();
 
-  fetch("https://api.github.com/")
-
+  useEffect(() => {
+    fetchLangs();
+  }, []);
   return (
     <Container className="__container">
       <h1>Projects</h1>
-      <ProjectEntry />
+      <ProjectEntry langs={langs} title={title} author={author} repoLink={repo}/>
       <Pagination count={10} color="primary" />
     </Container>
   );
+
+  function fetchLangs() {
+    fetch("https://api.github.com/repos/radeflex/freelrepos")
+      .then((resp) => resp.json())
+      .then((data) => {
+        fetch(data.languages_url)
+          .then((resp) => resp.json())
+          .then((langs) => {
+            setLangs(Object.keys(langs));
+          });
+        setAuthor(data.owner.login);
+        console.log(data.owner.description);
+        setTitle(data.description)
+        setRepo(data.html_url)
+      });
+  }
 }
