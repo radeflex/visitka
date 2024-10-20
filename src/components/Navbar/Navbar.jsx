a0e1332428f21278c4fb4c80b21c19b4c7874c64
Radeflex <maks.krav61@gmail.com>
2024-10-20 22:56:56 +0300

    made SPA redirect and some styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 import "./Navbar.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar_() {
+  const navigate = useNavigate();
   return (
     <div>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand>Radeflex</Navbar.Brand>
+          <Navbar.Brand onClick={() => {navigate("/contacts")}} style={{fontWeight: 700, fontSize: "20px", cursor: "pointer"}}>Radeflex</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/contacts">Contacts</Nav.Link>
+            <Nav.Link onClick={() => {navigate("/")}}>Home</Nav.Link>
+            <Nav.Link onClick={() => {navigate("/projects")}}>Projects</Nav.Link>
+            <Nav.Link onClick={() => {navigate("/contacts")}}>Contacts</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
