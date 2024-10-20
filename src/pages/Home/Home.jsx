b1da2897a0839ed54e3bd0262f5379dc2e0c8d27
Radeflex <maks.krav61@gmail.com>
2024-10-20 22:57:44 +0300

    made SPA redirect in button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import "./Home.css";
 import { Button, Container } from "react-bootstrap";
 import thumbup from "../../img/thumb-up.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <div>
       <Container className="_container">
         <section className="text-block">
           <h1 className="title">Hello there!</h1>
           <p className="text">I hope you will like this application!</p>
-          <Button variant="outline-success" href="/projects">
+          <Button variant="outline-success" onClick={() => {navigate("/projects")}}>
             Go to projects
           </Button>
         </section>
