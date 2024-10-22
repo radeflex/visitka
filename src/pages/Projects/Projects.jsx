49965c96aa24a8eaca569ba3d5518ff0fb708d4e
Radeflex <maks.krav61@gmail.com>
2024-10-22 18:10:20 +0300

    some addaptive added

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -3,6 +3,9 @@ import "./Projects.css";
 import ProjectEntry from "../../components/ProjectEntry/ProjectEntry";
 import { Container, Pagination } from "@mui/material";
 import PESkeleton from "../../components/ProjectEntry/Skeleton/PESkeleton";
+import visitkaImg from "../../img/visitka.png";
+import freelreposImg from "../../img/freelrepos.png";
+import directorsImg from "../../img/directors.png";
 
 const projects = [
   "https://api.github.com/repos/radeflex/freelrepos",
@@ -10,12 +13,15 @@ const projects = [
   "https://api.github.com/repos/radeflex/visitka",
 ];
 
+const imgs = [freelreposImg, directorsImg, visitkaImg];
+
 export default function Projects() {
   const [title, setTitle] = useState();
   const [author, setAuthor] = useState();
   const [langs, setLangs] = useState([]);
   const [currentPage, setPage] = useState(0);
   const [repo, setRepo] = useState();
+  const [img, setImg] = useState();
 
   useEffect(() => {
     fetchRepo(currentPage);
@@ -28,6 +34,7 @@ export default function Projects() {
         title={title}
         author={author}
         repoLink={repo}
+        img={img}
       />
       {/* <PESkeleton /> */}
       <Pagination
@@ -52,6 +59,7 @@ export default function Projects() {
         setAuthor(data.owner.login);
         setTitle(data.description);
         setRepo(data.html_url);
+        setImg(imgs[key]);
       });
   }
 }
