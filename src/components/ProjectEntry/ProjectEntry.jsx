e867bfda5316b82e604f862377fb5194fbdb7f26
Radeflex <maks.krav61@gmail.com>
2024-10-22 18:09:48 +0300

    some adaptive added

--- src/components/ProjectEntry/ProjectEntry.jsx
+++ src/components/ProjectEntry/ProjectEntry.jsx
@@ -2,24 +2,24 @@ import React from "react";
 import "./ProjectEntry.css";
 import Divider from "@mui/material/Divider";
 import { Card, Box, Stack, Chip, Typography, Button } from "@mui/material";
-import gift from "../../img/gift-box.png";
 
-export default function ProjectEntry({ langs, author, title, repoLink }) {
+export default function ProjectEntry({ langs, author, title, repoLink, img }) {
   return (
     <Card
       variant="outlined"
       sx={{
-        maxWidth: "100%",
-        maxHeight: "85%",
+        height: "100%",
+        width: "100%",
         borderRadius: "20px",
         boxShadow: "1px 2px 2px #c9c9c9",
+        margin: "1%"
       }}
     >
-      <Box sx={{ p: 3 }}>
-        <img src={gift} alt="project" height="30%" width="20%" />
+      <Box sx={{ p: 1 }}>
+        <img src={img} alt="project"  width="100%" />
         <Divider />
         <Box sx={{ p: 5 }}>
-          <Stack direction="column" spacing={0.5} alignItems="flex-start">
+          <Stack direction="column" spacing={0.25} alignItems="flex-start">
             <Typography variant="h5">{title}</Typography>
             <Typography variant="h6">Author: {author}</Typography>
             <Typography variant="h6">Languages:</Typography>
